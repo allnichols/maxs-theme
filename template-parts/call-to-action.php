8c2af1547565ab576201f3cccf3f4c942e6a6ceb
allnichols <allenmaxnichols@gmail.com>
2019-04-30 08:40:33 -0500

    position changes

--- template-parts/call-to-action.php
+++ template-parts/call-to-action.php
@@ -36,8 +36,8 @@
       <section class="container form">  
         <h1>Say Hello!</h1>
         <div class="icons">
-          <i class="fab fa-github-square"></i>
-          <i class="fab fa-linkedin"></i>
+          <a href="#!"><i class="fab fa-github-square"></i></a>
+          <a href="#!"><i class="fab fa-linkedin"></i></a>
         </div>
         <?php the_content(); ?>
       </section>
