b498f5b4543bba7316313f4898c2f366309a2efb
allnichols <allenmaxnichols@gmail.com>
2019-05-01 23:38:19 -0500

    form changes

--- template-parts/call-to-action.php
+++ template-parts/call-to-action.php
@@ -34,13 +34,6 @@
 
 <div class="form-section">
 
-  <section id="contact" class="container form">
-    <h1>Say Hello!</h1>
-    <div class="icons">
-      <a href="#!"><i class="fab fa-github-square"></i></a>
-      <a href="#!"><i class="fab fa-linkedin"></i></a>
-    </div>
-    <?php the_content(); ?>
-  </section>
+<?php get_template_part('template-parts/content', 'form') ?>
 
 </div>
